e794592e1c5a50c02048467abbaeddf29826413c
unknown <sinhashrey2206@gmail.com>
2021-07-02 22:33:03 +0530

    done

--- THA12/todo.js
+++ THA12/todo.js
@@ -1,22 +1,28 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
+function make_cross(){
+  var myNodelist = document.getElementsByTagName("LI");
+  var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+  span.classList.add('close');
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
+  // var tick=document.createElement('SPAN');
+  // tick.classList.add('tick');
+  //  tick.innerHTML='&#10004;';
+  // myNodelist[i].appendChild(tick);
 }
-
+}
+make_cross();
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
+var close = document.querySelectorAll(".close");
 var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+close.forEach((closeit)=>{
+  closeit.addEventListener('click',()=>{
+    var divi = closeit.parentElement;
+    divi.remove();
+  })
+})
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
@@ -38,17 +44,5 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  make_cross();
 }
