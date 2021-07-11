51b81c3d6a2372119538bda29dfdd6c227b7d4f3
unknown <sinhashrey2206@gmail.com>
2021-07-11 17:50:18 +0530

    done

--- THA18/tha18/src/App.js
+++ THA18/tha18/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+
+function  Box1() {
+  return <div style={{background:'white',
+  width:50,
+  height:50,
+  margin: 0
+}}> </div>;
+}
+
+function  Box2() {
+  return <div style={{background:'black',
+  width:50,
+  height:50,
+  margin: 0
+}}> </div>;
+}
+
+function Rows(){
+return (
+  <div style={{ display: 'flex',
+    margin: 0}}>
+      <Box1/>
+      <Box2/>
+      <Box1/>
+      <Box2/>
+      <Box1/>
+      <Box2/>
+      <Box1/>
+      <Box2/>
+  </div>
+ )
+}
+
+function Rows1(){
+  return (
+      <div  style={{ display:'flex',
+        margin: 0}}>
+          <Box2/>
+          <Box1/>
+          <Box2/>
+          <Box1/>
+          <Box2/>
+          <Box1/>
+          <Box2/>
+          <Box1/>
+      </div>
+     )
+  }
+
+function  Chess() {
+  const grey="#000";
+  return (
+  <div  style={{width: 399,
+  height: 399,
+  padding: 'none',
+  marginLeft: 600,
+  marginTop:100,
+  boxShadow: `0 0 6px 2px ${grey}`}}>
+     <Rows/>
+     <Rows1/>
+     <Rows/>
+     <Rows1/>
+     <Rows/>
+     <Rows1/>
+     <Rows/>
+     <Rows1/>
+  </div>
+  )
+}
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Chess/>
     </div>
   );
 }
